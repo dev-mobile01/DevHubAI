1455463735d54f5e0396bac65dc28630ec96f502
dev01 <esi.momeni1367@gmail.com>
2026-06-15 11:58:37 +0330

    Initial Commit

---
+++ DevHubAIAppTests/DevHubAIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevHubAIAppTests.swift
+//  DevHubAIAppTests
+//
+//  Created by ESI on 6/15/26.
+//
+
+import Testing
+@testable import DevHubAIApp
+
+struct DevHubAIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
